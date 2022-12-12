99cceda94c46b0472d258d1f925b2faea8e381e0
darthVader <extinctCoder@outlook.com>
2022-12-12 17:28:15 +0600

    configurator and logger added

---
+++ src/config.py
@@ -0,0 +1,19 @@
+from dynaconf import Dynaconf
+
+settings_files = ['conf/basic.yaml',
+                  'conf/settings.yaml',
+                  'conf/development.yaml',
+                  'conf/production.yaml']
+
+settings = Dynaconf(
+    warn_dynaconf_global_settings=True,
+    environments=True,
+    lowercase_read=False,
+    load_dotenv=True,
+    settings_files=settings_files,
+)
+
+if (settings.DEBUG):
+    settings.configure(FORCE_ENV_FOR_DYNACONF='development')
+else:
+    settings.configure(FORCE_ENV_FOR_DYNACONF='production')
--- src/main.py
+++ src/main.py
@@ -1,33 +1,8 @@
+from config import settings
 from log_x import log_arbiter
-from pathlib import Path
-from os.path import join
-from hydra import main as config_parser
-from omegaconf import DictConfig, OmegaConf
 
-# @config_parser(version_base=None, config_path=str(join(Path(__file__).resolve().parent.parent, 'conf')), config_name="config")
-# def main_app(cfg: DictConfig) -> None:
-#     '''Main entry point of the project. Only prints hello world to the console.
-#
-#     Returns:
-#         None: Returns Nothing
-#     '''
-#     print(OmegaConf.to_object(cfg)['app']['development'])
-#     return None
+logger = log_arbiter(__name__)
 
-
-@config_parser(version_base=None, config_path=str(join(Path(__file__).resolve().parent.parent, 'conf')), config_name='config')
-def main_app(cfg: DictConfig) -> None:
-    '''Main entry point of the project. Only prints hello world to the console.
-    Returns:
-        None: Returns Nothing
-    '''
-    logger = log_arbiter(logger_name=__name__, cfg=cfg)
-    print(logger)
-    # logger = logger.log_arbiter()
-    # logger.info('hello world')
-    return None
-
-
-# test done with hydra
 if __name__ == '__main__':
-    main_app()
+    logger.info('hello world ... ... ... :)')
+    logger.error('hello world is not working ... ... ... :)')
